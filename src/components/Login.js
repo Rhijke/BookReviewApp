3600c669970df14b38e53ad24e2621a2e9a3a281
Kaerah Lopez <kaerah.lopez@gmail.com>
2019-07-18 18:44:37 -0400

    Create login/create account form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
-
+import './Login.css';
 export class Login extends Component {
   render() {
     return (
       <div>
         <h3 className="page-header">Please login.</h3>
+        <form>
+          <div className="form-group">
+            <label>Email</label>
+            <input
+              className="form-control"
+              placeholder="Email"
+              name="email"
+              type="text"
+            />
+          </div>
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              className="form-control"
+              placeholder="Password"
+              name="password"
+              type="password"
+            />
+          </div>
+          <button className="btn btn-dark">Create Account</button>
+          <button className="btn btn-dark">Submit</button>
+        </form>
       </div>
     );
   }
