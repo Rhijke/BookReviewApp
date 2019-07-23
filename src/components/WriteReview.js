deab153932e56de028d1aa5896ed90ecdedf674e
Kaerah Lopez <kaerah.lopez@gmail.com>
2019-07-23 17:06:52 -0400

    Create form inputs for write a review page

--- src/components/WriteReview.js
+++ src/components/WriteReview.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
-
+import './css/WriteReview.css';
 export class WriteReview extends Component {
   render() {
     return (
       <div>
-        <h3 className="page-header"> Write a review</h3>
+        <h3 className="page-header">Write a review</h3>
+        <form method="POST">
+          <div className="form-group form-rating">
+            <label>Rating</label>
+            <select class="form-control">
+              <option>&#xf005;</option>
+              <option>&#xf005; &#xf005;</option>
+              <option>&#xf005; &#xf005; &#xf005;</option>
+              <option>&#xf005; &#xf005; &#xf005; &#xf005;</option>
+              <option>&#xf005; &#xf005; &#xf005; &#xf005; &#xf005;</option>
+            </select>
+          </div>
+          <div className="form-group form-review">
+            <label>Review</label>
+            <textarea
+              type="text"
+              rows="10"
+              className="form-control"
+              id="formGroupExampleInput2"
+              placeholder="Write your review here."
+            />
+          </div>
+          <button type="submit" class="btn btn-primary">
+            Submit
+          </button>
+        </form>
       </div>
     );
   }
