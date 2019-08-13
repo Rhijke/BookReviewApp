ce1a307a9cb6319b51fc2b788a575cf5a1ad569d
Kaerah Lopez <kaerah.lopez@gmail.com>
2019-08-13 16:11:14 -0400

    Change Write Review class to funcional component

--- client/src/components/WriteReview.js
+++ client/src/components/WriteReview.js
@@ -1,38 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './css/WriteReview.css';
-export class WriteReview extends Component {
-  render() {
-    return (
-      <div>
-        <h3 className="page-header">Write a review</h3>
-        <form method="POST">
-          <div className="form-group form-rating">
-            <label>Rating</label>
-            <select className="form-control">
-              <option>&#xf005;</option>
-              <option>&#xf005; &#xf005;</option>
-              <option>&#xf005; &#xf005; &#xf005;</option>
-              <option>&#xf005; &#xf005; &#xf005; &#xf005;</option>
-              <option>&#xf005; &#xf005; &#xf005; &#xf005; &#xf005;</option>
-            </select>
-          </div>
-          <div className="form-group form-review">
-            <label>Review</label>
-            <textarea
-              type="text"
-              rows="10"
-              className="form-control"
-              id="formGroupExampleInput2"
-              placeholder="Write your review here."
-            />
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </form>
-      </div>
-    );
-  }
-}
+// https://www.goodreads.com/review.xml
+export const WriteReview = props => {
+  const [review, setReview] = useState('');
+  const [rating, setRating] = useState(0);
+  const [bookId, setBookId] = useState();
+  useEffect(() => {
+    console.log(props);
+  }, []);
+  return (
+    <div>
+      <h3 className="page-header">Write a review</h3>
+      <form method="POST">
+        <div className="form-group form-rating">
+          <label>Rating</label>
+          <select className="form-control">
+            <option>&#xf005;</option>
+            <option>&#xf005; &#xf005;</option>
+            <option>&#xf005; &#xf005; &#xf005;</option>
+            <option>&#xf005; &#xf005; &#xf005; &#xf005;</option>
+            <option>&#xf005; &#xf005; &#xf005; &#xf005; &#xf005;</option>
+          </select>
+        </div>
+        <div className="form-group form-review">
+          <label>Review</label>
+          <textarea
+            type="text"
+            rows="10"
+            className="form-control"
+            id="formGroupExampleInput2"
+            placeholder="Write your review here."
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default WriteReview;
